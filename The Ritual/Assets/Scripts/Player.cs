ae95ac4aa749b7ceb91722f003b8726e902a35f2
Steven Stewart <stewart.steven98@outlook.com>
2018-10-18 17:35:25 -0400

    Added some changes in the player script

--- The Ritual/Assets/Scripts/Player.cs
+++ The Ritual/Assets/Scripts/Player.cs
@@ -23,9 +23,7 @@ public class Player : MonoBehaviour
     public Transform attackPos;
     public LayerMask whatIsEnemies;
     public int damage;
-    private float timeBtwPower;
-    public float startTimeBtwPower;
-    public float PowerRange;
+
 
     void Start()
     {
@@ -88,32 +86,14 @@ public class Player : MonoBehaviour
 
         }*/
 
-        if (Input.GetKeyDown(KeyCode.Space) && timeBtwPower <= 0)
-        {
-            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, PowerRange, whatIsEnemies);
-            for (int i = 0; i < enemiesToDamage.Length; i++)
-            {
-                enemiesToDamage[i].GetComponent<EnemyAICombat>().TakeDamage(damage);
-
-            }
-            timeBtwPower = startTimeBtwPower;
-        }
-        else
-        {
-            timeBtwPower -= Time.deltaTime;
-        }
-
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Bounds")
         {
             Debug.Log("Its hit bitch");
-            SceneManager.LoadScene("OpeningScene");
-        }
-        if(collision.tag == "Foe")
-        {
-            SceneManager.LoadScene("Combat");
+            gm.LoadScene();
+           //SceneManager.LoadScene("OpeningScene");
         }
 
     }
